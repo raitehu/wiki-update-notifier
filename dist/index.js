25639b5e8c063be05f253386ea707dc11c5907ab
Raitehu KIRISHIMA <pleiades-union@dune.ocn.ne.jp>
2022-04-12 22:21:56 +0900

    テストアプリの作成

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+// jsonデータを扱う
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+// テスト用のエンドポイント
+app.get('/', (req, res) => {
+    res.status(200).send({ message: 'hello, api sever!' });
+});
+// サーバー接続
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log('listen on port:', port);
+});
+//# sourceMappingURL=index.js.map

062894a7148f3cfff78a0f4988c0a2120b3722ad
Raitehu KIRISHIMA <pleiades-union@dune.ocn.ne.jp>
2022-04-12 23:59:55 +0900

    環境変数で指定されたURLのH1タグの内容を出力できるようにした

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv").config();
+const request = require("request");
+const cheerio = require("cheerio");
+const Iconv = require("iconv").Iconv;
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 // jsonデータを扱う
@@ -10,7 +14,27 @@ app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 // テスト用のエンドポイント
 app.get('/', (req, res) => {
-    res.status(200).send({ message: process.env.ROOT_PATH });
+    const options = {
+        url: process.env.ROOT_PATH,
+        method: "GET",
+        encoding: null,
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
+        }
+    };
+    request(options, (err, response, body) => {
+        if (err) {
+            console.error(err);
+        }
+        try {
+            const $ = cheerio.load(new Iconv("euc-jp", "UTF-8//TRANSLIT//IGNORE").convert(body).toString());
+            const title = $("h1").text();
+            res.status(200).send({ message: title });
+        }
+        catch (err) {
+            console.error(err);
+        }
+    });
 });
 // サーバー接続
 const port = process.env.PORT || 3001;
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
+require("dotenv").config()
+
+const request = require("request")
+const cheerio = require("cheerio")
+const Iconv   = require("iconv").Iconv
+
+import { AnyRecord } from 'dns';
 import express from 'express';
 
 const app = express()
@@ -7,11 +14,32 @@ app.use(express.urlencoded({ extended: true }))
 
 // テスト用のエンドポイント
 app.get('/', (req, res) => {
-    res.status(200).send({ message: process.env.ROOT_PATH })
+  const options = {
+    url: process.env.ROOT_PATH,
+    method: "GET",
+    encoding: null,
+    headers: {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
+    }
+  }
+
+  request(options, (err: Error, response: String, body: String) => {
+    if (err) {
+      console.error(err)
+    }
+
+    try {
+      const $ = cheerio.load(new Iconv("euc-jp", "UTF-8//TRANSLIT//IGNORE").convert(body).toString())
+      const title = $("h1").text()
+      res.status(200).send({ message: title })
+    } catch (err) {
+      console.error(err)
+    }
+  })
 })
 
 // サーバー接続
 const port = process.env.PORT || 3001
 app.listen(port, () => {
-    console.log('listen on port:', port)
+  console.log('listen on port:', port)
 })

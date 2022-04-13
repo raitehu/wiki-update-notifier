18fe1c72ce7b18fc4de11513e752390efbed574a
Raitehu KIRISHIMA <pleiades-union@dune.ocn.ne.jp>
2022-04-13 23:35:10 +0900

    関数の切り出し

--- dist/main.js
+++ dist/main.js
@@ -9,51 +9,37 @@ const request = require("request");
 const cheerio = require("cheerio");
 const Iconv = require("iconv").Iconv;
 const moment = require("moment");
+const yesterday = moment().add(-1, "days").format("YYYY-MM-DD");
 const RootPath = process.env.ROOT_PATH ? process.env.ROOT_PATH : "";
 const TwApiKey = process.env.API_KEY ? process.env.API_KEY : "";
 const TwApiKeySecret = process.env.API_KEY_SECRET ? process.env.API_KEY_SECRET : "";
 const TwAccessToken = process.env.ACCESS_TOKEN ? process.env.ACCESS_TOKEN : "";
 const TwAccessTokenSecret = process.env.ACCESS_TOKEN_SECRET ? process.env.ACCESS_TOKEN_SECRET : "";
-const options = {
-    url: RootPath,
-    method: "GET",
-    encoding: null,
-    headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
-    }
-};
-request(options, (err, response, body) => {
+request(requestOptions(), (err, response, body) => {
     if (err) {
         console.error(err);
     }
     try {
         const $ = cheerio.load(new Iconv("euc-jp", "UTF-8//TRANSLIT//IGNORE").convert(body).toString());
-        const title = $("h1").text();
-        const yesterday = moment().add(-1, "days").format("YYYY-MM-DD");
         let yesterdayUpdates = getSpecificDateUpdates(yesterday, getRecentUpdates($));
         if (yesterdayUpdates.length) {
-            // TODO: API認証キー周りは別関数に切り出し
-            const client = new twitter_api_v2_1.default({
-                appKey: TwApiKey,
-                appSecret: TwApiKeySecret,
-                accessToken: TwAccessToken,
-                accessSecret: TwAccessTokenSecret,
-            });
-            // TODO: message構築は別関数に切り出し
-            const messages = [
-                "🎪VALIS非公式wiki更新情報🎪",
-                `${yesterday}に以下のページが更新されました`,
-                yesterdayUpdates.map(updates => `・${updates.title}`),
-                "是非遊びに来てください!!",
-                process.env.ROOT_PATH ? process.env.ROOT_PATH : ""
-            ].flat();
-            client.v2.tweet(messages.join("\n"));
+            const client = new twitter_api_v2_1.default(twAuthentication());
+            client.v2.tweet(buildMessage(yesterday, yesterdayUpdates));
         }
     }
     catch (err) {
         console.error(err);
     }
 });
+function buildMessage(date, yesterdayUpdates) {
+    return [
+        "🎪VALIS非公式wiki更新情報🎪",
+        `${date}に以下のページが更新されました`,
+        yesterdayUpdates.map(updates => `・${updates.title}`),
+        "是非遊びに来てください!!",
+        RootPath
+    ].flat().join("\n");
+}
 function getRecentUpdates($) {
     let updatesGroupedByDate = [];
     $("#extra .side-box.recent ul.parent-list").children().each(function () {
@@ -83,5 +69,23 @@ function getSpecificDateUpdates(date, updates) {
     else {
         return [];
     }
+}
+function requestOptions() {
+    return {
+        url: RootPath,
+        method: "GET",
+        encoding: null,
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
+        }
+    };
+}
+function twAuthentication() {
+    return {
+        appKey: TwApiKey,
+        appSecret: TwApiKeySecret,
+        accessToken: TwAccessToken,
+        accessSecret: TwAccessTokenSecret,
+    };
 }
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -7,56 +7,40 @@ const cheerio = require("cheerio")
 const Iconv   = require("iconv").Iconv
 const moment  = require("moment")
 
+const yesterday: string           = moment().add(-1, "days").format("YYYY-MM-DD")
 const RootPath: string            = process.env.ROOT_PATH ? process.env.ROOT_PATH : ""
 const TwApiKey: string            = process.env.API_KEY ? process.env.API_KEY : ""
 const TwApiKeySecret: string      = process.env.API_KEY_SECRET ? process.env.API_KEY_SECRET : ""
 const TwAccessToken: string       = process.env.ACCESS_TOKEN ? process.env.ACCESS_TOKEN : ""
 const TwAccessTokenSecret: string = process.env.ACCESS_TOKEN_SECRET ? process.env.ACCESS_TOKEN_SECRET : ""
 
-const options = {
-  url: RootPath,
-  method: "GET",
-  encoding: null,
-  headers: {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
-  }
-}
-
-request(options, (err: Error, response: String, body: String) => {
+request(requestOptions(), (err: Error, response: String, body: String) => {
   if (err) {
     console.error(err)
   }
 
   try {
     const $ = cheerio.load(new Iconv("euc-jp", "UTF-8//TRANSLIT//IGNORE").convert(body).toString())
-    const title = $("h1").text()
-    const yesterday: String = moment().add(-1, "days").format("YYYY-MM-DD")
     let yesterdayUpdates = getSpecificDateUpdates(yesterday, getRecentUpdates($))
 
     if (yesterdayUpdates.length) {
-      // TODO: API認証キー周りは別関数に切り出し
-      const client = new Twitter({
-        appKey: TwApiKey,
-        appSecret: TwApiKeySecret,
-        accessToken: TwAccessToken,
-        accessSecret: TwAccessTokenSecret,
-      })
-
-      // TODO: message構築は別関数に切り出し
-      const messages: String[] = [
-        "🎪VALIS非公式wiki更新情報🎪",
-        `${yesterday}に以下のページが更新されました`,
-        yesterdayUpdates.map(updates => `・${updates.title}`),
-        "是非遊びに来てください!!",
-        process.env.ROOT_PATH ? process.env.ROOT_PATH : ""
-      ].flat()
-
-      client.v2.tweet(messages.join("\n"))
+      const client = new Twitter(twAuthentication())
+      client.v2.tweet(buildMessage(yesterday, yesterdayUpdates))
     }
   } catch (err) {
     console.error(err)
   }
 })
+
+function buildMessage(date: string, yesterdayUpdates: Page[]): string {
+  return [
+    "🎪VALIS非公式wiki更新情報🎪",
+    `${date}に以下のページが更新されました`,
+    yesterdayUpdates.map(updates => `・${updates.title}`),
+    "是非遊びに来てください!!",
+    RootPath
+  ].flat().join("\n")
+}
 
 function getRecentUpdates($: any): Updates[] {
   let updatesGroupedByDate: Updates[] = []
@@ -90,6 +74,26 @@ function getSpecificDateUpdates(date: String, updates: Updates[]): Page[] | [] {
     return found.pages
   } else {
     return []
+  }
+}
+
+function requestOptions(): object {
+  return {
+    url: RootPath,
+    method: "GET",
+    encoding: null,
+    headers: {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
+    }
+  }
+}
+
+function twAuthentication(): any {
+  return {
+    appKey:       TwApiKey,
+    appSecret:    TwApiKeySecret,
+    accessToken:  TwAccessToken,
+    accessSecret: TwAccessTokenSecret,
   }
 }
 

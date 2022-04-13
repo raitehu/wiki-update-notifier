d1f3e43861b51f91b22662473d3bcffd7f2e4ee0
Raitehu KIRISHIMA <pleiades-union@dune.ocn.ne.jp>
2022-04-13 22:46:56 +0900

    Twitterへの投稿機能実装と非API化

--- dist/main.js
+++ dist/main.js
@@ -8,57 +8,65 @@ const request = require("request");
 const cheerio = require("cheerio");
 const Iconv = require("iconv").Iconv;
 const moment = require("moment");
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
+const RootPath = process.env.ROOT_PATH ? process.env.ROOT_PATH : "";
+const TwApiKey = process.env.API_KEY ? process.env.API_KEY : "";
+const TwApiKeySecret = process.env.API_KEY_SECRET ? process.env.API_KEY_SECRET : "";
+const TwAccessToken = process.env.ACCESS_TOKEN ? process.env.ACCESS_TOKEN : "";
+const TwAccessTokenSecret = process.env.ACCESS_TOKEN_SECRET ? process.env.ACCESS_TOKEN_SECRET : "";
+//import express from "express"
+const twitter_api_v2_1 = __importDefault(require("twitter-api-v2"));
+//const app = express()
 // jsonデータを扱う
-app.use(express_1.default.json());
-app.use(express_1.default.urlencoded({ extended: true }));
+//app.use(express.json())
+//app.use(express.urlencoded({ extended: true }))
 // テスト用のエンドポイント
-app.get('/', (req, res) => {
-    const options = {
-        url: process.env.ROOT_PATH,
-        method: "GET",
-        encoding: null,
-        headers: {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
+//app.get('/', (req, res) => {
+//})
+const options = {
+    url: RootPath,
+    method: "GET",
+    encoding: null,
+    headers: {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
+    }
+};
+request(options, (err, response, body) => {
+    if (err) {
+        console.error(err);
+    }
+    try {
+        const $ = cheerio.load(new Iconv("euc-jp", "UTF-8//TRANSLIT//IGNORE").convert(body).toString());
+        const title = $("h1").text();
+        const yesterday = moment().add(-1, "days").format("YYYY-MM-DD");
+        let yesterdayUpdates = getSpecificDateUpdates(yesterday, getRecentUpdates($));
+        if (yesterdayUpdates.length) {
+            // TODO: API認証キー周りは別関数に切り出し
+            const client = new twitter_api_v2_1.default({
+                appKey: TwApiKey,
+                appSecret: TwApiKeySecret,
+                accessToken: TwAccessToken,
+                accessSecret: TwAccessTokenSecret,
+            });
+            // TODO: message構築は別関数に切り出し
+            const messages = [
+                "🎪VALIS非公式wiki更新情報🎪",
+                `${yesterday}に以下のページが更新されました`,
+                yesterdayUpdates.map(updates => `・${updates.title}`),
+                "是非遊びに来てください!!",
+                process.env.ROOT_PATH ? process.env.ROOT_PATH : ""
+            ].flat();
+            client.v2.tweet(messages.join("\n"));
         }
-    };
-    request(options, (err, response, body) => {
-        if (err) {
-            console.error(err);
-        }
-        try {
-            const $ = cheerio.load(new Iconv("euc-jp", "UTF-8//TRANSLIT//IGNORE").convert(body).toString());
-            const title = $("h1").text();
-            const yesterday = moment().add(-1, "days").format("YYYY-MM-DD");
-            let yesterdayUpdates = getSpecificDateUpdates(yesterday, getRecentUpdates($));
-            if (yesterdayUpdates.length) {
-                // 投稿用の文字列の構築
-                // Twitterへの投稿
-            }
-            res.status(200).send({ message: title, pages: getRecentUpdates($) });
-        }
-        catch (err) {
-            console.error(err);
-        }
-    });
+        //res.status(200).send({ message: title , pages: getRecentUpdates($)})
+    }
+    catch (err) {
+        console.error(err);
+    }
 });
-// サーバー接続
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-    console.log('listen on port:', port);
-});
-function getPages($) {
-    let wikiPages = [];
-    $("#sub .user-area li").each(function () {
-        let wikiPage = {
-            "title": $(this).children().html(),
-            "path": $(this).children().attr("href").replace(process.env.ROOT_PATH, "")
-        };
-        wikiPages.push(wikiPage);
-    });
-    return wikiPages;
-}
+//const port = process.env.PORT || 3001
+//app.listen(port, () => {
+//  console.log('listen on port:', port)
+//})
 function getRecentUpdates($) {
     let updatesGroupedByDate = [];
     $("#extra .side-box.recent ul.parent-list").children().each(function () {
--- src/main.ts
+++ src/main.ts
@@ -5,65 +5,74 @@ const cheerio = require("cheerio")
 const Iconv   = require("iconv").Iconv
 const moment  = require("moment")
 
-import express from 'express';
+const RootPath: string            = process.env.ROOT_PATH ? process.env.ROOT_PATH : ""
+const TwApiKey: string            = process.env.API_KEY ? process.env.API_KEY : ""
+const TwApiKeySecret: string      = process.env.API_KEY_SECRET ? process.env.API_KEY_SECRET : ""
+const TwAccessToken: string       = process.env.ACCESS_TOKEN ? process.env.ACCESS_TOKEN : ""
+const TwAccessTokenSecret: string = process.env.ACCESS_TOKEN_SECRET ? process.env.ACCESS_TOKEN_SECRET : ""
 
-const app = express()
+//import express from "express"
+import Twitter from "twitter-api-v2"
+
+//const app = express()
 // jsonデータを扱う
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+//app.use(express.json())
+//app.use(express.urlencoded({ extended: true }))
 
 // テスト用のエンドポイント
-app.get('/', (req, res) => {
-  const options = {
-    url: process.env.ROOT_PATH,
-    method: "GET",
-    encoding: null,
-    headers: {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
-    }
+//app.get('/', (req, res) => {
+//})
+const options = {
+  url: RootPath,
+  method: "GET",
+  encoding: null,
+  headers: {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
+  }
+}
+
+request(options, (err: Error, response: String, body: String) => {
+  if (err) {
+    console.error(err)
   }
 
-  request(options, (err: Error, response: String, body: String) => {
-    if (err) {
-      console.error(err)
+  try {
+    const $ = cheerio.load(new Iconv("euc-jp", "UTF-8//TRANSLIT//IGNORE").convert(body).toString())
+    const title = $("h1").text()
+    const yesterday: String = moment().add(-1, "days").format("YYYY-MM-DD")
+    let yesterdayUpdates = getSpecificDateUpdates(yesterday, getRecentUpdates($))
+
+    if (yesterdayUpdates.length) {
+      // TODO: API認証キー周りは別関数に切り出し
+      const client = new Twitter({
+        appKey: TwApiKey,
+        appSecret: TwApiKeySecret,
+        accessToken: TwAccessToken,
+        accessSecret: TwAccessTokenSecret,
+      })
+
+      // TODO: message構築は別関数に切り出し
+      const messages: String[] = [
+        "🎪VALIS非公式wiki更新情報🎪",
+        `${yesterday}に以下のページが更新されました`,
+        yesterdayUpdates.map(updates => `・${updates.title}`),
+        "是非遊びに来てください!!",
+        process.env.ROOT_PATH ? process.env.ROOT_PATH : ""
+      ].flat()
+
+      client.v2.tweet(messages.join("\n"))
     }
 
-    try {
-      const $ = cheerio.load(new Iconv("euc-jp", "UTF-8//TRANSLIT//IGNORE").convert(body).toString())
-      const title = $("h1").text()
-      const yesterday: String = moment().add(-1, "days").format("YYYY-MM-DD")
-      let yesterdayUpdates = getSpecificDateUpdates(yesterday, getRecentUpdates($))
-
-      if (yesterdayUpdates.length) {
-        // 投稿用の文字列の構築
-        // Twitterへの投稿
-      }
-
-      res.status(200).send({ message: title , pages: getRecentUpdates($)})
-    } catch (err) {
-      console.error(err)
-    }
-  })
+    //res.status(200).send({ message: title , pages: getRecentUpdates($)})
+  } catch (err) {
+    console.error(err)
+  }
 })
 
-// サーバー接続
-const port = process.env.PORT || 3001
-app.listen(port, () => {
-  console.log('listen on port:', port)
-})
-
-function getPages($: any): Array<object> {
-  let wikiPages: Array<object> = []
-
-  $("#sub .user-area li").each(function(this: String) {
-    let wikiPage = {
-      "title": $(this).children().html(),
-      "path" : $(this).children().attr("href").replace(process.env.ROOT_PATH, "")
-    }
-    wikiPages.push(wikiPage)
-  })
-  return wikiPages
-}
+//const port = process.env.PORT || 3001
+//app.listen(port, () => {
+//  console.log('listen on port:', port)
+//})
 
 function getRecentUpdates($: any): Updates[] {
   let updatesGroupedByDate: Updates[] = []
